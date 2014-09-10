c10f4f83e1b6cfd4bf9c3bbed98cfc2909d4159c
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-10 17:07:53 -0400

    Add BCrypt, has_secure_password, and password_digest.

--- db/migrate/20140910145818_create_users.rb
+++ db/migrate/20140910145818_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.name
-    t.password #something goes here for authentication
+      t.string :password_digest
     end
   end
 end
