aeb0e92a9ae24e0d24afd733478b1a0d1e57269b
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-11 13:25:42 -0400

    Add a Second User with Recipe to Seed File.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 require 'csv'
 
-jenn = User.create(name: "jenn", password_digest: "password")
-
-jenn.recipes << Recipe.create(name: "applesauce")
-
 CSV.foreach('db/nutrient_data_abbrev.csv', headers: true, header_converters: :symbol) do |food_item|
   FoodItem.create(name: "#{food_item[:shrt_desc]}", total_fat: food_item[:lipid_tot_g].to_f, total_protein: food_item[:protein_g].to_f, total_carb: food_item[:carbohydrt_g].to_f)
 end
+
+jenn = User.create(name: "jenn", password_digest: "password")
+jenn.recipes << Recipe.create(name: "applesauce")
+Recipe.first.ingredients << Ingredient.create(food_item_id: 50, quantity: 7, unit: "whole")
+
+paul = User.create(name: "paul", password_digest: "password")
+paul.recipes << Recipe.create(name: "carrot cake")
