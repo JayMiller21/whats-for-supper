423bfea03efd8a97e1d9fef3488edd2be00f5eea
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-11 19:12:07 -0400

    Add Notes About AJAX from Conversation with Coach.

--- public/js/application.js
+++ public/js/application.js
@@ -6,4 +6,34 @@ $(document).ready(function() {
 
   // TODO: Use AJAX to post ingredient data to database from recipe form after user adds additional ingredient fields. (Is this an appropriate use of AJAX? To post data from fields that weren't there when page was initially loaded?)
 
+  // Notes from talking to coach, Jared.
+
+  // ajax is used to add content to the current page without reloading.
+
+  // Example: I would write something like the following to edit a recipe and have the edits show up on the same page without refresh.
+
+  $('#new_recipe').on('submit',
+    function(e) {
+      // prevent default (post in ruby controller and redirect/reload)
+      e.preventDefault();
+
+      //assign needed vars
+      var name = $("#id").val(); // .val gets value contents of tag
+
+      // ajax call: says to get, post, etc. to the route specified. Then ruby implements whatever is in its controller for the route (i.e. saving data to database)
+      $.ajax({
+        method:'post',
+        url:'route goes here'
+        data: {recipe: name: r name},
+        dataType: JSON
+
+      //when that's done... the following will happen.
+      }).success( function(data)
+      $("#recipe_list").append(<li>asjdflajsdf</li>)
+      )
+
+  });
+
+  // Object orientedness comes in when there is repetitive code in the above. Can create objects, functions for these objects, and variables outside of the document ready, and call them from inside document ready.
+
 });
