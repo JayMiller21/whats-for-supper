2b1fd55fb68004d3b5df93a1808f1b24e51f0f28
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-24 17:46:13 -0400

    Create user dashboard route.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,38 +1,29 @@
-before "/user/*" do
-# before %r{/user/([\d]+)([\/]?)([\w]+)} do
-  current_user
-  @hello = "hello"
-  # @page_owner = User.find(params[:id]) #QUESTION: I couldn't figure out how I could refer to params[:id] in here.
-end
+# before "/user/*" do
+# # before %r{/user/([\d]+)([\/]?)([\w]+)} do
+#   current_user
+#   @hello = "hello"
+#   # @page_owner = User.find(params[:id]) #QUESTION: I couldn't figure out how I could refer to params[:id] in here.
+# end
 
-get "/user/new" do
-  erb :"user/new"
+before "/user/:id" do
+  current_user
 end
 
 get "/user/:id" do
   @user_id = params[:id].to_i
   @user = User.find(@user_id)
   @recipes = @user.recipes
+  p session
   erb :"/user/show"
 end
 
-post "/user/new" do
-  unless User.find_by(name: params[:user][:name])
-
-    user = User.create(name: params[:user][:name], password: params[:user][:password])
-    session[:error] = nil
-    redirect to "/user/#{user.id}"
+get "/user/:id/dashboard" do
+  @user_id = params[:id].to_i
+  @user = User.find(@user_id)
+  @recipes = @user.recipes
+  if @user_id == session[:current_user_id]
+    erb :"/user/dashboard"
+  else
+    redirect to :"/user/#{@user_id}"
   end
-    session[:error] = "User with that name already exists."
-    redirect to "/login"
-end
-
-get "/user/:id/edit" do
-
-  erb :"user/show"
-end
-
-get "/user/:id/recipes" do
-
-  erb :"recipe/index"
 end
