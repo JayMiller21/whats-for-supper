34631053a31152ca8df16587dacfe79e3e213827
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-24 17:42:35 -0400

    Update new recipe form to work synchronously with one ingredient field.

--- app/controllers/recipe.rb
+++ app/controllers/recipe.rb
@@ -4,10 +4,10 @@ get '/recipes' do
   erb :'recipe/index'
 end
 
-# get '/recipe/new' do
-#   @ingredient_counter = 1
-#   erb :'recipe/new'
-# end
+get '/recipe/new' do
+  @ingredient_counter = 1
+  erb :'recipe/new'
+end
 
 get '/recipe/:id' do
   @recipe = Recipe.find(params[:id])
@@ -20,18 +20,18 @@ get '/recipe/:id' do
   erb :'recipe/show'
 end
 
-# post '/recipe/new' do
-#   #can only add one ingredient currently
-#   @recipe = Recipe.create(name: params[:recipe][:name], author_id: session[:current_user_id])
-#   # binding.pry
-#   @ingredient = Ingredient.create(
-#     recipe_id: @recipe.id,
-#     quantity: params[:recipe][:ingredient][:quantity],
-#     unit: params[:recipe][:ingredient][:unit],
-#     food_item_id: params[:recipe][:ingredient][:food_item_id]
-#   )
-#   redirect to :"/recipe/#{@recipe.id}"
-# end
+post '/recipe/new' do
+  #can only add one ingredient currently
+  @recipe = Recipe.create(name: params[:recipe][:name], author_id: session[:current_user_id])
+  # binding.pry
+  @ingredient = Ingredient.create(
+    recipe_id: @recipe.id,
+    quantity: params[:recipe][:ingredient][:quantity],
+    unit: params[:recipe][:ingredient][:unit],
+    food_item_id: params[:recipe][:ingredient][:food_item_id]
+  )
+  redirect to :"/recipe/#{@recipe.id}"
+end
 
 # get '/recipe/:id/edit' do
 
