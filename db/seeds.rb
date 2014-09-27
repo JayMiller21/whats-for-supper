1c9806f58fc332161bd0675097247c488132e253
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-26 20:37:06 -0400

    Add seed comments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,6 @@ paul.recipes << Recipe.create(name: "carrot cake")
 
 
 # User.second.recipes.create(name: "salad", total_fat: nil, total_protein: nil, total_carb: nil)
+
+User.first.comments.create(content: "Delicious!", recipe_id: 2)
+User.second.comments.create(content: "Yuck!", recipe_id: 1)
