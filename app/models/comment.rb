f40a0c0d3952dd3a4964774ea2f22a702b35d76c
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-09-26 20:27:56 -0400

    Add comment model and corresponding associations.

---
+++ app/models/comment.rb
@@ -0,0 +1,4 @@
+class Comment < ActiveRecord::Base
+  belongs_to :owner, class_name: "User"
+  belongs_to :recipe
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   has_many :ingredients
+  has_many :comments
 
   # TODO: add description/ cooking instructions field.
 
